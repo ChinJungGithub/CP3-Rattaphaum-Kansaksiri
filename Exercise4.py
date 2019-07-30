3da284220d0c292a1dd6993a8479b65d18e716c4
ChinJungGithub <51363686+ChinJungGithub@users.noreply.github.com>
2019-07-30 07:57:18 +0700

    Create Exercise4.py

---
+++ Exercise4.py
@@ -0,0 +1,11 @@
+ENG=90
+Bussiness=80
+introCOM=95
+programCOM=99
+
+print("RESULT EXAM")
+print("...........")
+print("Foundation English"+ENG)
+print("General Business"+Business)
+print("Introduction to Ccmputer System"+introCOM)
+print("Computer programming"+programCOM)
